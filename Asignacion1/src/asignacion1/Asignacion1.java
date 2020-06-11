1ef146c2e8b9cb4d0aa7321d24217acd2658ab15
Zacarias18 <pvzacarias18@gmail.com>
2020-06-11 16:19:16 -0400

    primera asignacion1

---
+++ Asignacion1/src/asignacion1/Asignacion1.java
@@ -0,0 +1,14 @@
+package asignacion1;
+
+/**
+ *
+ * @author Familia Zacarias
+ */
+public class Asignacion1 
+{
+    public static void main(String[] args)
+    {
+        System.out.println("HOLA MUNDO");
+    }
+    
+}
