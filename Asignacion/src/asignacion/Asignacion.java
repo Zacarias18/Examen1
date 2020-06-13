d4be621973aa670ab7cce8c10eff7c48db2b9d2e
Zacarias18 <pvzacarias18@gmail.com>
2020-06-13 00:16:45 -0400

    cambio clase2

---
+++ Asignacion/src/asignacion/Asignacion.java
@@ -0,0 +1,16 @@
+
+package asignacion;
+
+/**
+ *
+ * @author Familia Zacarias
+ */
+public class Asignacion
+{  
+    public static void main(String[] args) 
+    {
+        System.out.println("HOLA MUNDO");
+        
+    }
+    
+}
